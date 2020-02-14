341a57c667d18dd43fa6a58568916e7a96c5d926
Oleksandr Savsunenko <o.savsunenko@ring.com>
2020-02-14 15:46:49 +0200

    code style fixes and hyper_opt now accept arguments

--- agent.py
+++ agent.py
@@ -1,36 +1,37 @@
-import numpy as np
 import random
-import pdb
 from collections import defaultdict
+
+import numpy as np
+
 
 class Agent:
 
-    def __init__(self, algorythm='sarsamax', start_epsilon=1, epsilon_decay=0.9, epsilon_cut=0.1, alpha=0.01, gamma=1, nA=6):
+    def __init__(self, algorithm='sarsamax', start_epsilon=1, epsilon_decay=0.9, epsilon_cut=0.1, alpha=0.01, gamma=1,
+                 nA=6):
         """ Initialize agent.
 
         Params
         ======
         - nA: number of actions available to the agent
         """
-        
+
         algos = {
             'sarsa': None,
             'sarsamax': self.step_sarmamax,
             'exp_sarsa': None
         }
-        
-        self.step = algos[algorythm]
-        self.Q = defaultdict(lambda: np.zeros(self.nA))    
-        self.epsilon, self.epsilon_decay, self.epsilon_cut, self.alpha, self.gamma, self.nA = start_epsilon, epsilon_decay, epsilon_cut, alpha, gamma, nA 
-    
+
+        self.step = algos[algorithm]
+        self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon, self.epsilon_decay, self.epsilon_cut, self.alpha, self.gamma, self.nA = \
+            start_epsilon, epsilon_decay, epsilon_cut, alpha, gamma, nA
 
     def select_action(self, state):
         r = random.random()
-        if r > self.epsilon: # select greedy action with probability epsilon
+        if r > self.epsilon:   # select greedy action with probability epsilon
             return np.argmax(self.Q[state])
-        else:                # otherwise, select an action randomly
-            return random.randint(0,5)
-        
+        else:  # otherwise, select an action randomly
+            return random.randint(0, 5)
 
     def step_sarmamax(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -43,10 +44,11 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        if not done: 
-            self.Q[state][action] += self.alpha * (reward + self.gamma * np.max(self.Q[next_state]) - self.Q[state][action])
+        if not done:
+            self.Q[state][action] += self.alpha * (
+                        reward + self.gamma * np.max(self.Q[next_state]) - self.Q[state][action])
         else:
-            self.Q[state][action] += self.alpha * (reward  - self.Q[state][action])
+            self.Q[state][action] += self.alpha * (reward - self.Q[state][action])
             self.epsilon = self.epsilon * self.epsilon_decay
             if self.epsilon_cut is not None:
                 self.epsilon = max(self.epsilon, self.epsilon_cut)
--- hyper_opt.py
+++ hyper_opt.py
@@ -1,45 +1,48 @@
+import argparse
+
+import gym
+from hyperopt import hp, fmin, tpe, space_eval
+
 from agent import Agent
 from monitor import interact
-import gym
-import pdb
-import numpy as np
-import hyperopt
-from hyperopt import hp
 
+parser = argparse.ArgumentParser(description='Hyper-opt settings')
+parser.add_argument('--n_iters', type=int, default=5,
+                    help='number of independent gym runs to get a mean for set of hyper parameters')
+parser.add_argument('--algo', default='sarsamax', help='algo to asses (sarsa, sarsamax, exp_sarsa)')
 
-N_ITERS = 5
+c_args = parser.parse_args()
+
 
 def objective(args):
     env = gym.make('Taxi-v2')
-    
+
     best_scores = []
-    for i in range(N_ITERS):
+    for i in range(c_args.n_iters):
         agent = Agent(
-            alpha=args[0], 
-            start_epsilon=args[1], 
-                      epsilon_decay=args[2], 
-                      epsilon_cut=None if args[3][0]==None else args[3][1], 
-                      gamma=args[4])
+            algorithm=c_args.algo,
+            alpha=args[0],
+            start_epsilon=args[1],
+            epsilon_decay=args[2],
+            epsilon_cut=None if args[3][0] is None else args[3][1],
+            gamma=args[4])
 
         avg_rewards, best_avg_reward = interact(env, agent, print_logs=False)
         best_scores.append(best_avg_reward)
 
-    return -sum(best_scores)/len(best_scores)
-    
+    return -sum(best_scores) / len(best_scores)
+
 
 # define a search space
 
 space = [hp.uniform('alpha', 0, 0.3),
-        hp.uniform('epsilon_start', 0.5, 1),
-        hp.uniform('epsilon_decay',0.8, 0.9999),
-        hp.choice('epsilon_cut', [
-            ('epsilon_cut_none', None),
-            ('epsilon_cut', hp.uniform('epsilon_cut_value', 0, 0.3))
-        ]),
-        hp.uniform('gamma', 0.3, 1)]
-
-# minimize the objective over the space
-from hyperopt import fmin, tpe, space_eval
+         hp.uniform('epsilon_start', 0.5, 1),
+         hp.uniform('epsilon_decay', 0.8, 0.9999),
+         hp.choice('epsilon_cut', [
+             ('epsilon_cut_none', None),
+             ('epsilon_cut', hp.uniform('epsilon_cut_value', 0, 0.3))
+         ]),
+         hp.uniform('gamma', 0.3, 1)]
 
 best = fmin(objective, space, algo=tpe.suggest, max_evals=100)
 print('Best params')
